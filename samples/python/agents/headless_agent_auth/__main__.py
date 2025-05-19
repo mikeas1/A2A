b74b5ea26a8338a9cc31486230253d5d56dd6946
Mike Smith <mikeas@google.com>
2025-05-19 22:58:57 +0000

    Update auth0 example to use security schemes

--- samples/python/agents/headless_agent_auth/__main__.py
+++ samples/python/agents/headless_agent_auth/__main__.py
@@ -1,31 +1,32 @@
 import asyncio
-import click
-import json
 import logging
 import os
 import sys
+
+import click
 import uvicorn
 
 from dotenv import load_dotenv
-load_dotenv()
 
-from agent import HRAgent
-from agent_executor import HRAgentExecutor
-from api import hr_api
-from oauth2_middleware import OAuth2Middleware
+
+load_dotenv()
 
 from a2a.server.apps import A2AStarletteApplication
 from a2a.server.request_handlers import DefaultRequestHandler
 from a2a.server.tasks import InMemoryTaskStore
 from a2a.types import (
-    AgentAuthentication,
     AgentCapabilities,
     AgentCard,
     AgentSkill,
-    # ClientCredentialsOAuthFlow,
-    # OAuth2SecurityScheme,
-    # OAuthFlows,
+    ClientCredentialsOAuthFlow,
+    OAuth2SecurityScheme,
+    OAuthFlows,
+    SecurityScheme,
 )
+from agent import HRAgent
+from agent_executor import HRAgentExecutor
+from api import hr_api
+from oauth2_middleware import OAuth2Middleware
 
 
 logging.basicConfig(level=logging.INFO)
@@ -66,33 +67,28 @@ async def start_agent(host: str, port):
                 ],
             )
         ],
-        authentication=AgentAuthentication(
-            schemes=['oauth2'],
-            credentials=json.dumps({
-                'tokenUrl': f'https://{os.getenv("HR_AUTH0_DOMAIN")}/oauth/token',
-                'scopes': {
-                    'read:employee_status': 'Allows confirming whether a person is an active employee of the company.'
-                }
-            }),
-        ),
-        # securitySchemes={
-        #     'oauth2_m2m_client': OAuth2SecurityScheme(
-        #         description='',
-        #         flows=OAuthFlows(
-        #             authorizationCode=ClientCredentialsOAuthFlow(
-        #                 tokenUrl=f'https://{os.getenv("HR_AUTH0_DOMAIN")}/oauth/token',
-        #                 scopes={
-        #                     'read:employee_status': 'Allows confirming whether a person is an active employee of the company.',
-        #                 },
-        #             ),
-        #         ),
-        #     ),
-        # },
-        # security=[{
-        #     'oauth2_m2m_client': [
-        #         'read:employee_status',
-        #     ],
-        # }],
+        securitySchemes={
+            'oauth2_m2m_client': SecurityScheme(
+                OAuth2SecurityScheme(
+                    description='',
+                    flows=OAuthFlows(
+                        clientCredentials=ClientCredentialsOAuthFlow(
+                            tokenUrl=f'https://{os.getenv("HR_AUTH0_DOMAIN")}/oauth/token',
+                            scopes={
+                                'read:employee_status': 'Allows confirming whether a person is an active employee of the company.',
+                            },
+                        ),
+                    ),
+                ),
+            )
+        },
+        security=[
+            {
+                'oauth2_m2m_client': [
+                    'read:employee_status',
+                ],
+            }
+        ],
     )
 
     request_handler = DefaultRequestHandler(
@@ -105,7 +101,11 @@ async def start_agent(host: str, port):
     )
 
     app = server.build()
-    app.add_middleware(OAuth2Middleware, agent_card=agent_card, public_paths=['/.well-known/agent.json'])
+    app.add_middleware(
+        OAuth2Middleware,
+        agent_card=agent_card,
+        public_paths=['/.well-known/agent.json'],
+    )
 
     logger.info(f'Starting HR Agent server on {host}:{port}')
     await uvicorn.Server(uvicorn.Config(app=app, host=host, port=port)).serve()
@@ -113,7 +113,9 @@ async def start_agent(host: str, port):
 
 async def start_api(host: str, port):
     logger.info(f'Starting HR API server on {host}:{port}')
-    await uvicorn.Server(uvicorn.Config(app=hr_api, host=host, port=port)).serve()
+    await uvicorn.Server(
+        uvicorn.Config(app=hr_api, host=host, port=port)
+    ).serve()
 
 
 # this ensures that `main()` runs when using `uv run .`
