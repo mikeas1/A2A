876cca61b94a1bee8659692a14811c3c82d47b57
Mike Smith <mikeas@google.com>
2025-05-20 13:05:22 +0000

    Nox formatting

--- samples/python/agents/headless_agent_auth/agent.py
+++ samples/python/agents/headless_agent_auth/agent.py
@@ -1,12 +1,14 @@
-from collections.abc import AsyncIterable
 import os
+
+from collections.abc import AsyncIterable
 from typing import Any, Literal
+
+import httpx
 
 from auth0.authentication.get_token import GetToken
 from auth0.management import Auth0
 from auth0_ai_langchain.auth0_ai import Auth0AI
 from auth0_ai_langchain.ciba import get_ciba_credentials
-import httpx
 from langchain_core.messages import AIMessage, ToolMessage
 from langchain_core.runnables.config import RunnableConfig
 from langchain_core.tools import tool
@@ -45,160 +47,161 @@ get_token = GetToken(
 
 @tool
 async def is_active_employee(employee_id: str) -> dict[str, Any]:
-  """Confirm whether a person is an active employee of the company.
+    """Confirm whether a person is an active employee of the company.
 
-  Args:
-      employee_id (str): The employee's identification.
+    Args:
+        employee_id (str): The employee's identification.
 
-  Returns:
-      dict: A dictionary containing the employment status, or an error message
-      if the request fails.
-  """
-  try:
-    credentials = get_ciba_credentials()
-    response = await httpx.AsyncClient().get(
-        f'{os.getenv("HR_API_BASE_URL", "http://localhost:10051")}/employees/{employee_id}',
-        headers={
-            'Authorization': (
-                f'{credentials["token_type"]} {credentials["access_token"]}'
-            ),
-            'Content-Type': 'application/json',
-        },
-    )
+    Returns:
+        dict: A dictionary containing the employment status, or an error message
+        if the request fails.
+    """
+    try:
+        credentials = get_ciba_credentials()
+        response = await httpx.AsyncClient().get(
+            f'{os.getenv("HR_API_BASE_URL", "http://localhost:10051")}/employees/{employee_id}',
+            headers={
+                'Authorization': (
+                    f'{credentials["token_type"]} {credentials["access_token"]}'
+                ),
+                'Content-Type': 'application/json',
+            },
+        )
 
-    if response.status_code == 404:
-      return {'active': False}
-    elif response.status_code == 200:
-      return {'active': True}
-    else:
-      response.raise_for_status()
-  except httpx.HTTPError as e:
-    return {'error': f'HR API request failed: {e}'}
-  except Exception as e:
-    return {'error': 'Unexpected response from HR API.'}
+        if response.status_code == 404:
+            return {'active': False}
+        if response.status_code == 200:
+            return {'active': True}
+        response.raise_for_status()
+    except httpx.HTTPError as e:
+        return {'error': f'HR API request failed: {e}'}
+    except Exception:
+        return {'error': 'Unexpected response from HR API.'}
 
 
 @tool
 def get_employee_id_by_email(work_email: str) -> dict[str, Any] | None:
-  """Return the employee ID by email.
+    """Return the employee ID by email.
 
-  Args:
-      work_email (str): The employee's work email.
+    Args:
+        work_email (str): The employee's work email.
 
-  Returns:
-      dict: A dictionary containing the employee ID if it exists, otherwise
-      None.
-  """
-  try:
-    user = Auth0(
-        domain=get_token.domain,
-        token=get_token.client_credentials(
-            f'https://{os.getenv("HR_AUTH0_DOMAIN")}/api/v2/'
-        )['access_token'],
-    ).users_by_email.search_users_by_email(
-        email=work_email, fields=['user_id']
-    )[
-        0
-    ]
+    Returns:
+        dict: A dictionary containing the employee ID if it exists, otherwise
+        None.
+    """
+    try:
+        user = Auth0(
+            domain=get_token.domain,
+            token=get_token.client_credentials(
+                f'https://{os.getenv("HR_AUTH0_DOMAIN")}/api/v2/'
+            )['access_token'],
+        ).users_by_email.search_users_by_email(
+            email=work_email, fields=['user_id']
+        )[0]
 
-    return {'employee_id': user['user_id']} if user else None
-  except Exception as e:
-    return {'error': 'Unexpected response from Auth0 Management API.'}
+        return {'employee_id': user['user_id']} if user else None
+    except Exception:
+        return {'error': 'Unexpected response from Auth0 Management API.'}
 
 
 class ResponseFormat(BaseModel):
-  """Respond to the user in this format."""
+    """Respond to the user in this format."""
 
-  status: Literal['completed', 'input-required', 'failed'] = 'failed'
-  message: str
+    status: Literal['completed', 'input-required', 'failed'] = 'failed'
+    message: str
 
 
 class HRAgent:
-  SUPPORTED_CONTENT_TYPES = ['text', 'text/plain']
+    SUPPORTED_CONTENT_TYPES = ['text', 'text/plain']
 
-  SYSTEM_INSTRUCTION: str = (
-      'You are an agent who handles external verification requests about Staff0'
-      ' employees made by third parties.Do not attempt to answer unrelated'
-      ' questions or use tools for other purposes.If you are asked about a'
-      " person's employee status using their employee ID, use the"
-      ' `is_active_employee` tool.If they provide a work email instead, first'
-      ' call the `get_employee_id_by_email` tool to get the employee ID, and'
-      ' then use `is_active_employee`.'
-  )
-
-  RESPONSE_FORMAT_INSTRUCTION: str = (
-      'Set the status to "completed" if the request has been fully'
-      ' processed.Set the status to "input-required" if the tool response'
-      ' indicates that user input is needed to proceed.If the tool response'
-      ' contains an AccessDeniedInterrupt error, set the message to "The user'
-      ' denied the authorization request", and set the status to "failed".For'
-      ' any other tool error, set the status to "failed".'
-  )
-
-  def __init__(self):
-    self.model = ChatGoogleGenerativeAI(model='gemini-2.0-flash')
-    self.tools = [
-        get_employee_id_by_email,
-        with_async_user_confirmation(is_active_employee),
-    ]
-
-    self.graph = create_react_agent(
-        self.model,
-        tools=self.tools,
-        checkpointer=MemorySaver(),
-        prompt=self.SYSTEM_INSTRUCTION,
-        response_format=(self.RESPONSE_FORMAT_INSTRUCTION, ResponseFormat),
+    SYSTEM_INSTRUCTION: str = (
+        'You are an agent who handles external verification requests about Staff0'
+        ' employees made by third parties.Do not attempt to answer unrelated'
+        ' questions or use tools for other purposes.If you are asked about a'
+        " person's employee status using their employee ID, use the"
+        ' `is_active_employee` tool.If they provide a work email instead, first'
+        ' call the `get_employee_id_by_email` tool to get the employee ID, and'
+        ' then use `is_active_employee`.'
     )
 
-  async def invoke(self, query: str, context_id: str) -> dict[str, Any]:
-    config: RunnableConfig = {'configurable': {'thread_id': context_id}}
-    await self.graph.ainvoke({'messages': [('user', query)]}, config)
-    return self.get_agent_response(config)
+    RESPONSE_FORMAT_INSTRUCTION: str = (
+        'Set the status to "completed" if the request has been fully'
+        ' processed.Set the status to "input-required" if the tool response'
+        ' indicates that user input is needed to proceed.If the tool response'
+        ' contains an AccessDeniedInterrupt error, set the message to "The user'
+        ' denied the authorization request", and set the status to "failed".For'
+        ' any other tool error, set the status to "failed".'
+    )
 
-  async def stream(
-      self, query: str, context_id: str
-  ) -> AsyncIterable[dict[str, Any]]:
-    inputs: dict[str, Any] = {'messages': [('user', query)]}
-    config: RunnableConfig = {'configurable': {'thread_id': context_id}}
+    def __init__(self):
+        self.model = ChatGoogleGenerativeAI(model='gemini-2.0-flash')
+        self.tools = [
+            get_employee_id_by_email,
+            with_async_user_confirmation(is_active_employee),
+        ]
 
-    async for item in self.graph.astream(inputs, config, stream_mode='values'):
-      message = item['messages'][-1] if 'messages' in item else None
-      if message:
-        if (
-            isinstance(message, AIMessage)
-            and message.tool_calls
-            and len(message.tool_calls) > 0
+        self.graph = create_react_agent(
+            self.model,
+            tools=self.tools,
+            checkpointer=MemorySaver(),
+            prompt=self.SYSTEM_INSTRUCTION,
+            response_format=(self.RESPONSE_FORMAT_INSTRUCTION, ResponseFormat),
+        )
+
+    async def invoke(self, query: str, context_id: str) -> dict[str, Any]:
+        config: RunnableConfig = {'configurable': {'thread_id': context_id}}
+        await self.graph.ainvoke({'messages': [('user', query)]}, config)
+        return self.get_agent_response(config)
+
+    async def stream(
+        self, query: str, context_id: str
+    ) -> AsyncIterable[dict[str, Any]]:
+        inputs: dict[str, Any] = {'messages': [('user', query)]}
+        config: RunnableConfig = {'configurable': {'thread_id': context_id}}
+
+        async for item in self.graph.astream(
+            inputs, config, stream_mode='values'
         ):
-          yield {
-              'is_task_complete': False,
-              'task_state': 'working',
-              'content': 'Looking up the employment status...',
-          }
-        elif isinstance(message, ToolMessage):
-          yield {
-              'is_task_complete': False,
-              'task_state': 'working',
-              'content': 'Processing the employment status...',
-          }
+            message = item['messages'][-1] if 'messages' in item else None
+            if message:
+                if (
+                    isinstance(message, AIMessage)
+                    and message.tool_calls
+                    and len(message.tool_calls) > 0
+                ):
+                    yield {
+                        'is_task_complete': False,
+                        'task_state': 'working',
+                        'content': 'Looking up the employment status...',
+                    }
+                elif isinstance(message, ToolMessage):
+                    yield {
+                        'is_task_complete': False,
+                        'task_state': 'working',
+                        'content': 'Processing the employment status...',
+                    }
 
-    yield self.get_agent_response(config)
+        yield self.get_agent_response(config)
 
-  def get_agent_response(self, config: RunnableConfig) -> dict[str, Any]:
-    current_state = self.graph.get_state(config)
-    structured_response = current_state.values.get('structured_response')
+    def get_agent_response(self, config: RunnableConfig) -> dict[str, Any]:
+        current_state = self.graph.get_state(config)
+        structured_response = current_state.values.get('structured_response')
 
-    if structured_response and isinstance(structured_response, ResponseFormat):
-      return {
-          'is_task_complete': structured_response.status == 'completed',
-          'task_state': structured_response.status,
-          'content': structured_response.message,
-      }
+        if structured_response and isinstance(
+            structured_response, ResponseFormat
+        ):
+            return {
+                'is_task_complete': structured_response.status == 'completed',
+                'task_state': structured_response.status,
+                'content': structured_response.message,
+            }
 
-    return {
-        'is_task_complete': False,
-        'task_state': 'unknown',
-        'content': (
-            'We are unable to process your request at the moment. Please try'
-            ' again.'
-        ),
-    }
+        return {
+            'is_task_complete': False,
+            'task_state': 'unknown',
+            'content': (
+                'We are unable to process your request at the moment. Please try'
+                ' again.'
+            ),
+        }
--- samples/python/agents/headless_agent_auth/test_client.py
+++ samples/python/agents/headless_agent_auth/test_client.py
@@ -1,8 +1,12 @@
 import asyncio
 import os
+
 from typing import Any
 from urllib.parse import urlparse
 from uuid import uuid4
+
+import asyncclick as click
+import httpx
 
 from a2a.client import A2ACardResolver, A2AClient
 from a2a.types import (
@@ -20,10 +24,8 @@ from a2a.types import (
     TaskStatusUpdateEvent,
     TextPart,
 )
-import asyncclick as click
 from auth0.authentication.get_token import GetToken
 from dotenv import load_dotenv
-import httpx
 
 
 load_dotenv()
@@ -31,42 +33,42 @@ access_token = None
 
 
 class AgentAuth(httpx.Auth):
-  """Custom httpx's authentication class to inject access token required by agent."""
+    """Custom httpx's authentication class to inject access token required by agent."""
 
-  def __init__(self, agent_card: AgentCard):
-    self.agent_card = agent_card
+    def __init__(self, agent_card: AgentCard):
+        self.agent_card = agent_card
 
-  def auth_flow(self, request):
-    global access_token
+    def auth_flow(self, request):
+        global access_token
 
-    client_creds_flow = next(
-        (
-            scheme.root.flows.clientCredentials
-            for scheme in self.agent_card.securitySchemes.values()
-            if isinstance(scheme.root, OAuth2SecurityScheme)
-            and scheme.root.flows.clientCredentials
-        ),
-        None,
-    )
-    if not client_creds_flow:
-      yield request
-      return
+        client_creds_flow = next(
+            (
+                scheme.root.flows.clientCredentials
+                for scheme in self.agent_card.securitySchemes.values()
+                if isinstance(scheme.root, OAuth2SecurityScheme)
+                and scheme.root.flows.clientCredentials
+            ),
+            None,
+        )
+        if not client_creds_flow:
+            yield request
+            return
 
-    if not access_token:
-      token_url = client_creds_flow.tokenUrl
-      print(f'\nFetching agent access token from {token_url}...')
-      get_token = GetToken(
-          domain=urlparse(token_url).hostname,
-          client_id=os.getenv('A2A_CLIENT_AUTH0_CLIENT_ID'),
-          client_secret=os.getenv('A2A_CLIENT_AUTH0_CLIENT_SECRET'),
-      )
-      access_token = get_token.client_credentials(
-          os.getenv('HR_AGENT_AUTH0_AUDIENCE')
-      )['access_token']
-      print('Done.\n')
+        if not access_token:
+            token_url = client_creds_flow.tokenUrl
+            print(f'\nFetching agent access token from {token_url}...')
+            get_token = GetToken(
+                domain=urlparse(token_url).hostname,
+                client_id=os.getenv('A2A_CLIENT_AUTH0_CLIENT_ID'),
+                client_secret=os.getenv('A2A_CLIENT_AUTH0_CLIENT_SECRET'),
+            )
+            access_token = get_token.client_credentials(
+                os.getenv('HR_AGENT_AUTH0_AUDIENCE')
+            )['access_token']
+            print('Done.\n')
 
-    request.headers['Authorization'] = f'Bearer {access_token}'
-    yield request
+        request.headers['Authorization'] = f'Bearer {access_token}'
+        yield request
 
 
 @click.command()
@@ -75,73 +77,75 @@ class AgentAuth(httpx.Auth):
 @click.option('--history', default=False, is_flag=True)
 @click.option('--debug', default=False, is_flag=True)
 async def cli(agent: str, context_id: str | None, history: bool, debug: bool):
-  async with httpx.AsyncClient() as httpx_client:
-    agent_card = await A2ACardResolver(
-        httpx_client=httpx_client,
-        base_url=agent,
-    ).get_agent_card()
+    async with httpx.AsyncClient() as httpx_client:
+        agent_card = await A2ACardResolver(
+            httpx_client=httpx_client,
+            base_url=agent,
+        ).get_agent_card()
 
-    print('======= Agent Card ========')
-    print(agent_card.model_dump_json(exclude_none=True, indent=2))
+        print('======= Agent Card ========')
+        print(agent_card.model_dump_json(exclude_none=True, indent=2))
 
-    httpx_client.auth = AgentAuth(agent_card)
+        httpx_client.auth = AgentAuth(agent_card)
 
-    client = A2AClient(
-        httpx_client=httpx_client,
-        agent_card=agent_card,
-    )
-
-    if not context_id:
-      context_id = uuid4().hex
-
-    continue_loop = True
-    streaming = agent_card.capabilities.streaming
-
-    while continue_loop:
-      task_id = uuid4().hex
-      print('=========  Starting a New Task ======== ')
-      continue_loop = await complete_task(
-          client,
-          streaming,
-          task_id,
-          context_id,
-          debug,
-      )
-
-      if history and continue_loop:
-        print('========= History ======== ')
-        get_task_response = await client.get_task(
-            GetTaskRequest(params=TaskQueryParams(id=task_id, historyLength=10))
+        client = A2AClient(
+            httpx_client=httpx_client,
+            agent_card=agent_card,
         )
-        print(
-            get_task_response.root.model_dump_json(
-                include={'result': {'history': True}}
+
+        if not context_id:
+            context_id = uuid4().hex
+
+        continue_loop = True
+        streaming = agent_card.capabilities.streaming
+
+        while continue_loop:
+            task_id = uuid4().hex
+            print('=========  Starting a New Task ======== ')
+            continue_loop = await complete_task(
+                client,
+                streaming,
+                task_id,
+                context_id,
+                debug,
             )
-        )
+
+            if history and continue_loop:
+                print('========= History ======== ')
+                get_task_response = await client.get_task(
+                    GetTaskRequest(
+                        params=TaskQueryParams(id=task_id, historyLength=10)
+                    )
+                )
+                print(
+                    get_task_response.root.model_dump_json(
+                        include={'result': {'history': True}}
+                    )
+                )
 
 
 def create_send_params(
     text: str, task_id: str | None = None, context_id: str | None = None
 ) -> MessageSendParams:
-  """Helper function to create the payload for sending a task."""
-  send_params: dict[str, Any] = {
-      'message': {
-          'role': 'user',
-          'parts': [{'kind': 'text', 'text': text}],
-          'messageId': uuid4().hex,
-      },
-      'configuration': {
-          'acceptedOutputModes': ['text'],
-      },
-  }
+    """Helper function to create the payload for sending a task."""
+    send_params: dict[str, Any] = {
+        'message': {
+            'role': 'user',
+            'parts': [{'kind': 'text', 'text': text}],
+            'messageId': uuid4().hex,
+        },
+        'configuration': {
+            'acceptedOutputModes': ['text'],
+        },
+    }
 
-  if task_id:
-    send_params['message']['taskId'] = task_id
+    if task_id:
+        send_params['message']['taskId'] = task_id
 
-  if context_id:
-    send_params['message']['contextId'] = context_id
+    if context_id:
+        send_params['message']['contextId'] = context_id
 
-  return MessageSendParams(**send_params)
+    return MessageSendParams(**send_params)
 
 
 async def complete_task(
@@ -151,104 +155,104 @@ async def complete_task(
     context_id: str,
     debug: bool = False,
 ) -> bool:
-  prompt = click.prompt(
-      '\nWhat do you want to send to the agent? (:q or quit to exit)'
-  )
-
-  if prompt == ':q' or prompt == 'quit':
-    return False
-
-  send_params = create_send_params(
-      text=prompt,
-      task_id=task_id,
-      context_id=context_id,
-  )
-
-  task = None
-  if streaming:
-    stream_response = client.send_message_streaming(
-        SendStreamingMessageRequest(params=send_params)
-    )
-    async for chunk in stream_response:
-      result = chunk.root.result
-      print(
-          f'stream event => {chunk.root.model_dump_json(exclude_none=True)}'
-          if debug
-          else (
-              next(
-                  (
-                      f'stream message => role: {result.role.value}, kind:'
-                      f' {part.root.kind}, text: {part.root.text}'
-                      for part in result.parts
-                      if isinstance(part.root, TextPart)
-                  ),
-                  '',
-              )
-              if isinstance(result, Message)
-              else next(
-                  (
-                      f'stream message => role: {msg.role.value}, kind:'
-                      f' {part.root.kind}, text: {part.root.text}'
-                      for msg in result.history or []
-                      for part in msg.parts
-                      if isinstance(part.root, TextPart)
-                  ),
-                  '',
-              )
-              if isinstance(result, Task)
-              else next(
-                  (
-                      'stream message => role:'
-                      f' {result.status.message.role.value}, kind:'
-                      f' {part.root.kind}, text: {part.root.text}'
-                      for part in (
-                          result.status.message.parts
-                          if result.status.message
-                          else []
-                      )
-                      if isinstance(part.root, TextPart)
-                  ),
-                  '',
-              )
-              if isinstance(result, TaskStatusUpdateEvent)
-              else next(
-                  (
-                      f'stream artifact => kind: {part.root.kind}, text:'
-                      f' {part.root.text}'
-                      for part in result.artifact.parts
-                      if isinstance(part.root, TextPart)
-                  ),
-                  '',
-              )
-              if isinstance(result, TaskArtifactUpdateEvent)
-              else ''
-          )
-      )
-
-    get_task_response = await client.get_task(
-        GetTaskRequest(params=TaskQueryParams(id=task_id))
-    )
-    task = get_task_response.root.result
-  else:
-    send_message_response = await client.send_message(
-        SendMessageRequest(params=send_params)
-    )
-    task = send_message_response.root.result
-    print(f'\n{task.model_dump_json(exclude_none=True)}')
-
-  # if the result is that more input is required, loop again.
-  if task.status.state == TaskState.input_required:
-    return await complete_task(
-        client,
-        streaming,
-        task_id,
-        context_id,
-        debug,
+    prompt = click.prompt(
+        '\nWhat do you want to send to the agent? (:q or quit to exit)'
     )
 
-  # task is complete
-  return True
+    if prompt == ':q' or prompt == 'quit':
+        return False
+
+    send_params = create_send_params(
+        text=prompt,
+        task_id=task_id,
+        context_id=context_id,
+    )
+
+    task = None
+    if streaming:
+        stream_response = client.send_message_streaming(
+            SendStreamingMessageRequest(params=send_params)
+        )
+        async for chunk in stream_response:
+            result = chunk.root.result
+            print(
+                f'stream event => {chunk.root.model_dump_json(exclude_none=True)}'
+                if debug
+                else (
+                    next(
+                        (
+                            f'stream message => role: {result.role.value}, kind:'
+                            f' {part.root.kind}, text: {part.root.text}'
+                            for part in result.parts
+                            if isinstance(part.root, TextPart)
+                        ),
+                        '',
+                    )
+                    if isinstance(result, Message)
+                    else next(
+                        (
+                            f'stream message => role: {msg.role.value}, kind:'
+                            f' {part.root.kind}, text: {part.root.text}'
+                            for msg in result.history or []
+                            for part in msg.parts
+                            if isinstance(part.root, TextPart)
+                        ),
+                        '',
+                    )
+                    if isinstance(result, Task)
+                    else next(
+                        (
+                            'stream message => role:'
+                            f' {result.status.message.role.value}, kind:'
+                            f' {part.root.kind}, text: {part.root.text}'
+                            for part in (
+                                result.status.message.parts
+                                if result.status.message
+                                else []
+                            )
+                            if isinstance(part.root, TextPart)
+                        ),
+                        '',
+                    )
+                    if isinstance(result, TaskStatusUpdateEvent)
+                    else next(
+                        (
+                            f'stream artifact => kind: {part.root.kind}, text:'
+                            f' {part.root.text}'
+                            for part in result.artifact.parts
+                            if isinstance(part.root, TextPart)
+                        ),
+                        '',
+                    )
+                    if isinstance(result, TaskArtifactUpdateEvent)
+                    else ''
+                )
+            )
+
+        get_task_response = await client.get_task(
+            GetTaskRequest(params=TaskQueryParams(id=task_id))
+        )
+        task = get_task_response.root.result
+    else:
+        send_message_response = await client.send_message(
+            SendMessageRequest(params=send_params)
+        )
+        task = send_message_response.root.result
+        print(f'\n{task.model_dump_json(exclude_none=True)}')
+
+    # if the result is that more input is required, loop again.
+    if task.status.state == TaskState.input_required:
+        return await complete_task(
+            client,
+            streaming,
+            task_id,
+            context_id,
+            debug,
+        )
+
+    # task is complete
+    return True
 
 
 if __name__ == '__main__':
-  asyncio.run(cli())
+    asyncio.run(cli())
